5da4677b4466f5304ba39716f6dfa55d83c2602a
Jonathan Turner <jonathan.d.turner@gmail.com>
2021-02-21 14:43:58 +1300

    Making an iterator

--- src/employee.rs
+++ src/employee.rs
@@ -1,5 +1,40 @@
 use crate::ided::Ided;
 
+#[derive(Debug)]
+pub struct EmployeeRecords {
+    idx: usize,
+    employees: Vec<Employee>,
+}
+
+impl EmployeeRecords {
+    pub fn new() -> EmployeeRecords {
+        EmployeeRecords {
+            idx: 0,
+            employees: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, employee: Employee) {
+        self.employees.push(employee);
+    }
+}
+
+impl Iterator for EmployeeRecords {
+    type Item = Employee;
+
+    // next() is the only required method
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.employees.len() > self.idx {
+            let output = self.employees[self.idx].clone();
+            self.idx += 1;
+            Some(output)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Employee {
     name: String,
     id: i64,
@@ -12,6 +47,10 @@ impl Employee {
 
     fn name(&self) -> &str {
         &self.name
+    }
+
+    fn update(&mut self) {
+        self.id = 100;
     }
 
     pub fn id(&self) -> i64 {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod employee;
-use employee::Employee;
+use employee::{Employee, EmployeeRecords};
 
 mod ided;
 use ided::use_ided;
@@ -8,11 +8,20 @@ mod student;
 use student::Student;
 
 fn main() {
-    let employee = Employee::new(String::from("Employee"), 100);
-    let student = Student { student_id: 300 };
+    let mut employee_records = EmployeeRecords::new();
+    employee_records.push(Employee::new("Jane".to_string(), 101));
+    employee_records.push(Employee::new("Joe".to_string(), 102));
+    println!("{:#?}", employee_records);
 
-    use_ided(Box::new(employee));
-    use_ided(Box::new(student));
+    for employee in employee_records {
+        println!("{:?}", employee);
+    }
+
+    // let employee = Employee::new(String::from("Employee"), 100);
+    // let student = Student { student_id: 300 };
+
+    // use_ided(Box::new(employee));
+    // use_ided(Box::new(student));
 
     // println!(
     //     "employee => name: {} id: {}",
